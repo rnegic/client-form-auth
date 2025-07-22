01fcaa87d57de735a37e8484d7a6e2f237683388
Марк Иванов <arnego666@gmail.com>
2025-07-22 13:30:09 +0300

    Feat/Added vite config, all deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    allowedHosts: ['localhost'],
+    port: 5173,
+    proxy: {
+      '/api': {
+        changeOrigin: true,
+        secure: false,
+        target: 'http://localhost:4000',
+      },
+    },
+    strictPort: true,
+  },
 })
